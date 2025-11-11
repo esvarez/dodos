2bc7f3b0bc2cf0a198e8384cdbe82f073020077e
Erick Suárez <42328586+esvarez@users.noreply.github.com>
2025-11-10 19:38:42 -0600

    Create input component (#1)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { InputComponent } from './components/input/input.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [InputComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'dodos';
+  emailValue = '';
 }
